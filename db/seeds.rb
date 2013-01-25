d6f8c600bc37be8f674277d526dee70ac108bbd2
Raghu Betina <rbetina@gmail.com>
2013-01-25 09:27:47 -0600

    Added seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pictures = [
+      { :url => "http://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png", :caption => "Ruby: Designed for developer happiness." },
+      { :url => "http://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png", :caption => "Rails: Agile web development." },
+      { :url => "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png", :caption => "HTML5: Powerful structure and presentation." }
+    ]
+
+pictures.each do |pic|
+  p = Picture.new
+  p.url = pic[:url]
+  p.caption = pic[:caption]
+  p.save
+end
