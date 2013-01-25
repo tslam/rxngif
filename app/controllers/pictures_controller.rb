4fb92ec9622536086328c7595bde67f482c361b5
Raghu Betina <rbetina@gmail.com>
2013-01-25 08:26:37 -0600

    Made the new action work; but it doesn't remember the URL.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,11 +2,20 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = Picture.all
+
+    if params[:url].present?
+      @pictures << params[:url]
+    end
+
   end
 
   def show
     @user_request = params["id"]
     @url = Picture.all[@user_request.to_i - 1]
+  end
+
+  def new
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rxngif::Application.routes.draw do
 
-  get("/pictures", { :controller => "Pictures", :action => "index" })
+  root :to => "Pictures#index"
+
+  # get("/pictures", { :controller => "Pictures", :action => "index" })
+  get "/pictures" => "Pictures#index"
+
+  get "/pictures/new" => "Pictures#new"
 
   get "/pictures/:id", :controller => "Pictures", :action => "show"
-
 end
