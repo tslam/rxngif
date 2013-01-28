5fe162cd81625b670933d986652ecf95cb6c5f4a
Raghu Betina <rbetina@gmail.com>
2013-01-28 09:43:05 -0600

    Started cleaning up our code with named route helper methods.

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,30 @@ Rxngif::Application.routes.draw do
   root :to => "Pictures#index"
 
   # Create
-  get "/pictures/new" => "Pictures#new"
+  get "/pictures/new" => "Pictures#new", :as => "new_picture_form"
   post "/pictures" => "Pictures#create"
 
   # Read
   get("/pictures", {:controller => "Pictures", :action => "index", :as => "pictures_list"})
 
+  # The :as => "pictures" option causes Rails to define the following method:
   # def pictures_url
       # return "http://localhost:3000/pictures"
   # end
 
   get "/pictures/:id" => "Pictures#show", :as => "single_picture"
 
+  # The :as => "picture" option causes Rails to define the following method:
+  # def picture_url(id_number)
+  #     return "http://localhost:3000/pictures/#{id_number}"
+  # end
+
+
   # Update
-  get "/pictures/:id/edit" => "Pictures#edit"
+  get "/pictures/:id/edit" => "Pictures#edit", :as => "edit_picture_form"
   post "/edit_picture/:id" => "Pictures#update"
 
   # Destroy
-  get "/delete_picture/:id" => "Pictures#delete"
+  get "/delete_picture/:id" => "Pictures#delete", :as => "delete_picture"
 
 end
